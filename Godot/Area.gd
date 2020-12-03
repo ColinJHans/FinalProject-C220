2791aa614bd68ee40ba37117ecdf2fafce50b114
mkareken <70339051+mkareken@users.noreply.github.com>
2020-12-03 14:32:04 -0500

    XXXXXXXDDDDDD

---
+++ Godot/Area.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+
+func _ready():
+	pass
+	
+	
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Player":
+		if Global.level == 1:
+			get_tree().change_scene("res://Scenes/Underwater_Level.tscn")
+		if Global.level == 2:
+			get_tree().change_scene("res://Scenes/Level3.tscn")
+		if Global.level == 3:
+			get_tree().change_scene("res://Scenes/Game Over.tscn")
--- Godot/Global.gd
+++ Godot/Global.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var level = 1
+
 func _unhandled_input(event):
 	if event.is_action_pressed("quit"):
 		get_tree().quit()
